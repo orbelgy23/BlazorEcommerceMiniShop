95f219efaa7e683af7ccd6aae5d61fcd80455ca6
Or Belsky <orbelsky@campus.technion.ac.il>
2022-05-28 16:37:26 +0300

    Added the product model, the product razor component, the web api (Get method only, including mock data), and the product service in the client side

---
+++ BlazorEcommerceMiniShop/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorEcommerceMiniShop.Shared;
+
+namespace BlazorEcommerceMiniShop.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+
+        public List<Product> Products { get; set; }
+
+        Task GetProducts();
+
+    }
+}
---
+++ BlazorEcommerceMiniShop/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorEcommerceMiniShop.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerceMiniShop.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        private readonly HttpClient _http;
+
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task GetProducts()
+        {
+            var result = await _http.GetFromJsonAsync<List<Product>>("api/product");
+            if (result != null)
+            {
+                Products = result;
+            }
+        }
+    }
+}
